e85d910c6564a3f768d1f7195fbea7af016eee3e
NathanielAddo <stevenelly100@gmail.com>
2025-04-10 05:28:47 +0000

    Refactor GreetingLottie to use dynamic import for Lottie and fetch animation data

--- components/DisplayLottie.tsx
+++ components/DisplayLottie.tsx
@@ -1,21 +1,41 @@
 import React from "react";
-import Lottie from 'lottie-react';
+import dynamic from 'next/dynamic';
+
+// Dynamic import with SSR disabled
+const Lottie = dynamic(
+  () => import('lottie-react'),
+  { 
+    ssr: false,
+    loading: () => <div className="h-64 w-64 bg-gray-200 rounded-lg animate-pulse" />
+  }
+);
 
 type Props = {
   animationPath: string;
 };
 
 const GreetingLottie = ({ animationPath }: Props) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    path: animationPath,
-  };
+  const [animationData, setAnimationData] = React.useState(null);
+
+  React.useEffect(() => {
+    // Fetch the animation data
+    fetch(animationPath)
+      .then(response => response.json())
+      .then(data => setAnimationData(data))
+      .catch(error => console.error('Error loading animation:', error));
+  }, [animationPath]);
+
+  if (!animationData) {
+    return <div className="h-64 w-64 bg-gray-200 rounded-lg animate-pulse" />;
+  }
 
   return (
-    <div onClick={() => null}>
-      {/* @ts-ignore */}
-      <Lottie options={defaultOptions} />
+    <div className="cursor-pointer">
+      <Lottie 
+        animationData={animationData}
+        loop={true}
+        autoplay={true}
+      />
     </div>
   );
 };
